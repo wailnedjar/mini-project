f71e146d69319fb1de7639e55d2f2a14d85f4cca
ahmed123456787 <124626957+ahmed123456787@users.noreply.github.com>
2023-12-14 22:41:19 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -4,10 +4,12 @@ import java.io.Serializable;
 
 public class Person implements Serializable{
    
-	String name , email , department_type ;
+	String first_name ,last_name , email , department_type ;
+	int age ;
 	
-	Person (String name , String email , String department_type){
-		this.name = name ; 
+	Person (String first_name ,String last_name , int age , String email , String department_type){
+		this.last_name = last_name ;
+		this.first_name = first_name ; 
 		this.email = email ;
 		this.department_type = department_type ;
 	}
