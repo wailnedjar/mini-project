ebb8097e6004caf4c253c9c6be48fe54fa710d0b
mehdibidjed <139001110+mehdibidjed@users.noreply.github.com>
2023-12-16 23:32:39 +0100

    Update Studentwail.java

--- Studentwail.java
+++ Studentwail.java
@@ -8,7 +8,15 @@ import java.util.*;
 		private int section,groupe;
 		private ArrayList<Module> ModuleStudy;
 		private  float GradeFinal;
+		private Cycle Cylce_Etud;
+		public Cycle getCyle() {
+		return Cycle_Etudes;
+	}
+	public void setCyle(Cycle_Etudes cyle) {
+		Cycle_Etudes = cyle;
+	}
 		public float getGradeFinal()
+			
 {
 return this.GradeFinal;
 }
@@ -16,6 +24,18 @@ public void SetGradeFinal(float X)
 {
 this.GradeFinal=X;
 }
+		
+		public void AffecterModule()
+		{
+			ArrayList<modules> AllModules=AllList.Modules;
+			for(Module M:AllModules )
+				{
+					if(M.Cycle.getNomCycle().equals(this.Cycle_Etud.getNomCycle()))
+					{
+						this.ModuleStudy.add(M);
+					}
+				}
+		}
 		public Student (String name,String gender,String phone,Date birth,int ID,Adresse adresse,String dress) {
 			super(name,gender,phone,birth,ID,adresse,dress
 			this.ModuleStudy=new ArrayList();
