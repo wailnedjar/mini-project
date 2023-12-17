9a5c76fdf9b2cb07b7e01a1c7df2af50778a08fe
wailnedjar <153867941+wailnedjar@users.noreply.github.com>
2023-12-17 09:07:35 +0100

    Update Adresse.java

--- Adresse.java
+++ Adresse.java
@@ -1,14 +1,17 @@
-package studentManagement;
+import java.util.Scanner;
 
-public class Adress {
-
+public class Adresse{
 	private String Country;
 	private String City;
 	private String Street;
 	private String houseNumber;
-	
-	
-	
+	public Adresse(String country, String city, String street, String houseNumber) {
+		Country = country;
+		City = city;
+		Street = street;
+		this.houseNumber = houseNumber;
+	}
+	public Adresse() {}
 	public String getCountry() {
 		return Country;
 	}
@@ -33,7 +36,29 @@ public class Adress {
 	public void setHouseNumber(String houseNumber) {
 		this.houseNumber = houseNumber;
 	}
-	
-	
-	
+	 public void setAdresse() {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Enter Country:");
+	        setCountry(scanner.nextLine());
+
+	        System.out.println("Enter City:");
+	        setCity(scanner.nextLine());
+
+	        System.out.println("Enter Street:");
+	        setStreet(scanner.nextLine());
+
+	        System.out.println("Enter House Number:");
+	        setHouseNumber(scanner.nextLine());
+
+	        scanner.close();
+	    }
+	 public void displayInfo() {
+	        System.out.println("Address Informations:");
+	        System.out.println("Country: " + Country);
+	        System.out.println("City: " + City);
+	        System.out.println("Street: " + Street);
+	        System.out.println("House Number: " + houseNumber);
+	    
+	 }
 }
